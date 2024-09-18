30f4059692c88ad60fa1dd0d6e15adf76ef236f3
kousum <kousumwork@outlook.com>
2024-09-18 14:23:25 +0800

    of

--- packages/vue/src/hooks/useSignal.ts
+++ packages/vue/src/hooks/useSignal.ts
@@ -1,24 +1,19 @@
-
-import {effect, signal, Signal} from '@dnd-kit/state';
-
-import {useConstant} from './useConstant.ts';
+import {effect, Signal} from '@dnd-kit/state';
 import {useIsomorphicLayoutEffect} from './useIsomorphicLayoutEffect.ts';
-import {getCurrentInstance, nextTick, ref, ShallowRef, shallowRef, unref} from 'vue';
+import {getCurrentInstance, nextTick, ShallowRef, shallowRef, toRaw} from 'vue';
 
 /** Wrap the given value in a Signal if it isn't already one, and make changes trigger a re-render. */
-export function useSignal<T = any>(signalOrValue: ShallowRef<T>, sync = ()=>false) {
-  const sig = useConstant(() =>
-    signalOrValue.value instanceof Signal ? signalOrValue.value : signal(signalOrValue.value)
-  );
-  let val = sig.value?.peek();
+export function useSignal<T = any>(signal: ShallowRef<Signal<T>>, sync = ()=>false) {
+
+  let val = toRaw(signal.value)?.peek();
   const update = shallowRef(val);
   const currentInstance = getCurrentInstance()
 
   useIsomorphicLayoutEffect(
     () =>
       effect(() => {
-        let val = unref(sig)?.peek();
-        if (val !== (val = unref(sig)?.value)) {
+        let val = toRaw(signal.value)?.peek();
+        if (val !== (val = toRaw(signal.value).value)) {
           if (sync()) {
             //TODO
             // flushSync(() => update.value = val);
@@ -29,7 +24,7 @@ export function useSignal<T = any>(signalOrValue: ShallowRef<T>, sync = ()=>fals
           }
         }
       }),
-    [sync]
+    [()=>signal, sync]
   );
-  return sig;
+  return signal;
 }
